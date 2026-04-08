c4f0bf2c41ae9231c14904eab3c969a39d182ef4
Filipa Ferreira <a22408989@alunos.ulht.pt>
2026-04-08 19:31:57 +0100

    implement InvalidInput class

--- src/pt/ulusofona/aed/deisiworldmeter/InvalidInput.java
+++ src/pt/ulusofona/aed/deisiworldmeter/InvalidInput.java
@@ -1,4 +1,47 @@
 package pt.ulusofona.aed.deisiworldmeter;
 
 public class InvalidInput {
+    /* attributes */
+    private String fileName;
+    private int numCorrectLines;
+    private int numIncorrectLines;
+    private int firstIncorrectLine;
+
+    /* default builder */
+    public InvalidInput(){
+
+    }
+
+    /* builders */
+    public InvalidInput(String fileName, int numCorrectLines, int numIncorrectLines, int firstIncorrectLine){
+        /* create object */
+        this.fileName = fileName;
+        this.numCorrectLines = numCorrectLines;
+        this.numIncorrectLines = numIncorrectLines;
+        this.firstIncorrectLine = firstIncorrectLine;
+    }
+
+    /* getters */
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getNumCorrectLines(){
+        return numCorrectLines;
+    }
+
+    public int getNumIncorrectLines(){
+        return numIncorrectLines;
+    }
+
+    public int getFirstIncorrectLine(){
+        return firstIncorrectLine;
+    }
+
+    /* methods */
+    @Override
+    public String toString() {
+        return fileName + ".csv | " + numCorrectLines + " | " +
+                numIncorrectLines + " | " + firstIncorrectLine;
+    }
 }
