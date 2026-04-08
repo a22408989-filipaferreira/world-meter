b54d76020ceae16b6dd53a23132cc5d0a4d165d5
Filipa Ferreira <a22408989@alunos.ulht.pt>
2026-04-08 19:45:28 +0100

    create the data storage lists in Main

--- src/pt/ulusofona/aed/deisiworldmeter/Main.java
+++ src/pt/ulusofona/aed/deisiworldmeter/Main.java
@@ -1,4 +1,13 @@
 package pt.ulusofona.aed.deisiworldmeter;
 
+import java.util.ArrayList;
+
 public class Main {
+    /* data storage structures */
+    private ArrayList<Country> listOfCountries = new ArrayList<>();
+    private ArrayList<City> listOfCities = new ArrayList<>();
+    private ArrayList<Population> listOfPopulations = new ArrayList<>();
+    private ArrayList<InvalidInput> listOfInvalidInputs = new ArrayList<>();
+
+
 }
